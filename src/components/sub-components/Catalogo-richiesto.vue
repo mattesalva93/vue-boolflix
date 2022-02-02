a7c7995e72c1ee2176b013157d0bda6c3947dc6a
mattesalva93 <salvalaggiomatteo93@gmail.com>
2022-02-02 17:49:05 +0100

    flags done

--- src/components/sub-components/Catalogo-richiesto.vue
+++ src/components/sub-components/Catalogo-richiesto.vue
@@ -2,7 +2,20 @@
     <ul>
         <li>{{film.title}}</li>
         <li>{{film.original_title}}</li>
-        <li>{{film.original_language}}</li>
+
+        <li v-if="film.original_language == 'it'"> 
+            <img src="../../assets/img/it-flag.png" alt=""> 
+        </li>
+        <li v-else-if="film.original_language == 'en'"> 
+            <img src="../../assets/img/en-flag.png" alt=""> 
+        </li>
+        <li v-else-if="film.original_language == 'es'"> 
+            <img src="../../assets/img/es-flag.png" alt=""> 
+        </li>
+        <li v-else>
+            <img src="../../assets/img/no-flag.png" alt="">
+        </li>
+
         <li>{{film.vote_average}}</li>
     </ul>
 </template>
@@ -17,5 +30,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+    img{
+        width: 25px;
+    }
 </style>
